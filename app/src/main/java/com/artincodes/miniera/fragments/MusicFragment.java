f609aa4d4605e35801f1ca3dc038c302e00f3c72
Jayadeep <sjaideep93@gmail.com>
2015-01-24 11:03:58 +0530

    first pull development branch

---
+++ app/src/main/java/com/artincodes/miniera/fragments/MusicFragment.java
@@ -0,0 +1,47 @@
+package com.artincodes.miniera.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.artincodes.miniera.MainActivity;
+import com.artincodes.miniera.R;
+
+public  class MusicFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MusicFragment newInstance(int sectionNumber) {
+        MusicFragment fragment = new MusicFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MusicFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
